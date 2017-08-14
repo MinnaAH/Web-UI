f97ed3859e0cb1b4084cc849b20c820dc6389911
Minna Hannula <m.hannula@elisanet.fi>
2017-08-14 22:15:50 +0300

    Etusivu

---
+++ etusivu.php
@@ -0,0 +1,13 @@
+<head>
+<title>Etusivu</title>
+</head
+<div class="text-center">
+ <h3 class="margin">Etusivu</h3>
+  <p>Täälä sivustolla nähdään ajankohtaista dataa säätiedoista. Taulukosta löytyy kaikkien säätietojen viimeisimmät 1000 mittausta</p>
+  <p>JSON välilehdiltä löytyy kahden eri säätiedon JSON koodit, joita on käytetty sivuja tehdessä</p>
+  <p>Tuulen nopeus kertoo ajankohtaisen päivittyvän tuulennopeuden.</p>
+  <p>Lämpötila kertoo viimeisen 24 tunnin säätiedot.</p>
+  <p>Ilmanpaineesta löytyy ajankohtainen päivittyvä tieto ilmanpaineesta.</p>
+  <p>Tuulensuunta kertoo ajankohtaisen tuulensuunnan.</p>
+  <p>Sääasema, josta tiedot saadaan sijaitsee Tampereen Ammattikorkeakoululla.</p>
+</div>
